7af5ebb07c95cf92dc4a24ab056d64da642fab98
OlivierR63 <olivier.rochat@gmail.com>
2025-07-09 10:42:59 -0400

    Move routes import to end of __init__.py to prevent circular imports and disable unused-import lint warning

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,4 +3,6 @@ from flask import Flask
 # Create Flask application
 app = Flask(__name__)
 
-# from backend import routes
+# Import routes after creating the app to avoid circular imports
+# This import is necessary to register the routes with the Flask app
+from backend import routes  # pylint: disable=unused-import
